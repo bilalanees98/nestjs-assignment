364d40100fe9207a48a2ccf3e9033958e24cbdac
muhammad_bilal <muhammad.bilal@tintash.com>
2021-09-10 16:46:35 +0500

    added user-user resursive association

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { isEmail } from 'validator';
+import * as mongoose from 'mongoose';
 export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
@@ -16,6 +17,9 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  following: User[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
