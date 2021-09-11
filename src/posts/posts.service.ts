e183d5f488bc111bdab953dae31928e47c30bf70
muhammad_bilal <muhammad.bilal@tintash.com>
2021-09-11 11:16:42 +0500

    shifted res sending from post srvice to ctrller

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -25,7 +25,7 @@ export class PostsService {
         .skip(offset ? parseInt(offset) : 0)
         .limit(limit ? parseInt(limit) : 0);
 
-      return { data: posts };
+      return posts;
     } catch (error) {
       return { error: error.toString() };
     }
@@ -35,7 +35,7 @@ export class PostsService {
     try {
       const post = await this.postModel.findOne({ _id: id });
 
-      return { data: post };
+      return post;
     } catch (error) {
       return { error: error.toString() };
     }
